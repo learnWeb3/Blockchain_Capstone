6127c4d0608f746f118df6f9a98c0a6fd6b35565
web3learn <antoinehadrienleguillou@outlook.com>
2021-09-02 11:26:09 +0200

    basic implementations done with tests

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,47 +1,95 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var CustomERC721Token = artifacts.require("CustomERC721Token");
+const assert = require("assert");
+const BigNumber = require("bignumber.js");
 
-contract('TestERC721Mintable', accounts => {
+contract("TestERC721Mintable", (accounts) => {
+  const account_one = accounts[0];
+  const account_two = accounts[1];
 
-    const account_one = accounts[0];
-    const account_two = accounts[1];
-
-    describe('match erc721 spec', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-
-            // TODO: mint multiple tokens
-        })
-
-        it('should return total supply', async function () { 
-            
-        })
-
-        it('should get token balance', async function () { 
-            
-        })
-
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            
-        })
-
-        it('should transfer token from one owner to another', async function () { 
-            
-        })
+  describe("match erc721 spec", function () {
+    beforeEach(async function () {
+      this.contract = await CustomERC721Token.new({ from: account_one });
+      // TODO: mint multiple tokens
+      await this.contract.mint(account_one, 1);
+      await this.contract.mint(account_two, 2);
     });
 
-    describe('have ownership properties', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-        })
-
-        it('should fail when minting when address is not contract owner', async function () { 
-            
-        })
-
-        it('should return contract owner', async function () { 
-            
-        })
-
+    it("should return total supply", async function () {
+      const totalSupply = await this.contract.totalSupply();
+      assert.equal(
+        new BigNumber(totalSupply).isEqualTo(new BigNumber(2)),
+        true
+      );
     });
-})
+
+    it("should get token balance", async function () {
+      const balance_account_one = await this.contract.balanceOf(account_one);
+      const balance_account_two = await this.contract.balanceOf(account_two);
+      assert.equal(
+        new BigNumber(balance_account_one).isEqualTo(new BigNumber(1)),
+        true
+      );
+      assert.equal(
+        new BigNumber(balance_account_two).isEqualTo(new BigNumber(1)),
+        true
+      );
+    });
+
+    // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
+    it("should return token uri", async function () {
+      const token_uri_account_one = await this.contract.tokenURI(1, {
+        from: account_one,
+      });
+      const token_uri_account_two = await this.contract.tokenURI(1, {
+        from: account_two,
+      });
+      assert.equal(
+        token_uri_account_one,
+        "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1"
+      );
+      assert.equal(
+        token_uri_account_two,
+        "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1"
+      );
+    });
+
+    it("should transfer token from one owner to another", async function () {
+      await this.contract.transferFrom(account_one, account_two, 1);
+      const balance_account_one = await this.contract.balanceOf(account_one);
+      const balance_account_two = await this.contract.balanceOf(account_two);
+      assert.equal(
+        new BigNumber(balance_account_one).isEqualTo(new BigNumber(0)),
+        true
+      );
+      assert.equal(
+        new BigNumber(balance_account_two).isEqualTo(new BigNumber(2)),
+        true
+      );
+      const eventLogs = await this.contract.getPastEvents("Transfer", {
+        fromBlock: 0,
+        filter: { from: account_one, to: account_two, tokenId: 1 },
+      });
+      assert.equal(eventLogs.length, 1);
+    });
+  });
+
+  describe("have ownership properties", function () {
+    beforeEach(async function () {
+      this.contract = await CustomERC721Token.new({ from: account_one });
+    });
+
+    it("should fail when minting when address is not contract owner", async function () {
+      try {
+        await this.contract.mint(account_one, 1, { from: account_two });
+        assert.fail();
+      } catch (error) {
+        assert.equal(error.reason, "caller must be owner");
+      }
+    });
+
+    it("should return contract owner", async function () {
+      const contract_owner = await this.contract.getOwner();
+      assert.equal(contract_owner, account_one);
+    });
+  });
+});
